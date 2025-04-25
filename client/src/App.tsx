79532c525ecad3c95dec7a7bb3be429f162445e9
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-25 13:17:19 +0300

    master:feat: initialize react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      <Button className="text-red-500">Hello</Button>
+    <div className="p-4 h-screen flex flex-col items-center justify-center">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
