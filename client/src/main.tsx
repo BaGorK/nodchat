368d16926c81c1163233f21c0569edd7e5a04b83
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-04-28 11:01:23 +0300

    master:feat: setting up socketio on the frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App.tsx';
 import { AuthContextProvider } from './providers/AuthContextProvider.tsx';
 import { Toaster } from 'react-hot-toast';
+import { SocketContextProvider } from './providers/SocketContextProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthContextProvider>
-      <App />
+      <SocketContextProvider>
+        <App />
+      </SocketContextProvider>
       <Toaster />
     </AuthContextProvider>
   </StrictMode>,
---
+++ client/src/providers/SocketContextProvider.tsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useEffect, useRef, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
+import { useAuthContext } from './AuthContextProvider';
+
+interface ISocketContext {
+  socket: Socket | null;
+  onlineUsers: string[];
+}
+
+const SocketContext = createContext<ISocketContext | null>(null);
+
+const socketURL = import.meta.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '/'; // '/' bc the frontend the backend will be deployed on the same server.
+
+export const SocketContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const socketRef = useRef<Socket | null>(null);
+  const [onlineUsers, setOnlineUsers] = useState<string[]>([]);
+  const { authUser, isLoading } = useAuthContext();
+
+  useEffect(() => {
+    if (authUser && !isLoading) {
+      const socket = io(socketURL, {
+        query: {
+          userId: authUser.id,
+        },
+      });
+
+      socketRef.current = socket;
+
+      socket.on('getOnlineUsers', (users: string[]) => {
+        setOnlineUsers(users);
+      });
+
+      return () => {
+        socket.close();
+        socketRef.current = null;
+      };
+    } else if (!authUser && !isLoading) {
+      if (socketRef.current) {
+        socketRef.current.close();
+        socketRef.current = null;
+      }
+    }
+  }, [authUser, isLoading]);
+
+  return <SocketContext.Provider value={{ socket: socketRef.current, onlineUsers }}>{children}</SocketContext.Provider>;
+};
+
+export const useSocketContext = () => {
+  const ctx = useContext(SocketContext);
+
+  if (!ctx) {
+    throw new Error('useSocketContext must be used within a SocketContextProvider');
+  }
+
+  return ctx;
+};
